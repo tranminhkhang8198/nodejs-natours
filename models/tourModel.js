4c5f7da9df369c66b7d28736199d99614e67ac10
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-01-14 17:03:34 +0700

    Document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -60,6 +61,20 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true });
+});
+
+// tourSchema.pre('save', function(next) {
+//     console.log('Will save document...');
+// });
+//
+// tourSchema.post('save', function(doc, next) {
+//     console.log(doc);
+//     next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
