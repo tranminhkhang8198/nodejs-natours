28b5a6fe18b85412f8d2ff16d0a95f737463331f
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-01-15 15:52:24 +0700

    Handling Invalid Database IDs

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 
-
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
