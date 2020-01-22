984f132f245526a5b4a4cbc86ad35b0d2d1438b7
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-01-22 16:48:14 +0700

    Password Reset Functionality Reset Token

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 
 const userSchema = new mongoose.Schema({
@@ -38,7 +39,9 @@ const userSchema = new mongoose.Schema({
             message: 'Passwords are not the same!'
         }
     },
-    passwordChangedAt: Date
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date
 });
 
 
@@ -68,6 +71,18 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
 
     // False means NOT changed
     return false;
+}
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+
+    console.log({ resetToken }, this.passwordResetToken);
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
 }
 
 const User = mongoose.model('User', userSchema);
