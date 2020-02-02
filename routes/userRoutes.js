ad327920ccd81dcec31280dc01647764f638e74c
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-02-02 11:09:11 +0700

    Features: update current user data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,8 @@ router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 router
     .route('/')
     .get(userController.getAllUsers)
