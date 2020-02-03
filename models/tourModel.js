8801fd2867bcd3ca29aa7816f20a6ca168d23cea
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-02-03 18:30:41 +0700

    Populating Tour Guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -124,11 +124,11 @@ tourSchema.pre('save', function(next) {
     next();
 });
 
-tourSchema.pre('save', async function(next) {
-    const guidesPromises = this.guides.map(async id => await User.findById(id));
-    this.guides = await Promise.all(guidesPromises);
-    next();
-});
+// tourSchema.pre('save', async function(next) {
+//     const guidesPromises = this.guides.map(async id => await User.findById(id));
+//     this.guides = await Promise.all(guidesPromises);
+//     next();
+// });
 
 // tourSchema.pre('save', function(next) {
 //     console.log('Will save document...');
@@ -146,6 +146,15 @@ tourSchema.pre('save', async function(next) {
 tourSchema.pre(/^find/, function(next) {
     this.find({ secretTour: { $ne: true } });
     this.start = Date.now();
+
+    next();
+});
+
+tourSchema.pre(/^find/, function(next) {
+    this.populate({
+        path: 'guides',
+        select: '-__v -passwordChangedAt'
+    });
 
     next();
 });
