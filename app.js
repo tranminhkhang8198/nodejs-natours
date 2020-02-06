580ad99e73d35284183829142c944827301f3a8a
tranminhkhang8198 <minhkhang811998@gmail.com>
2020-02-06 08:19:08 +0700

    First Steps with Pug

--- app.js
+++ app.js
@@ -54,7 +54,10 @@ app.use((req, res, next) => {
 
 // 2) ROUTES
 app.get('/', (req, res) => {
-    res.status(200).render('base');
+    res.status(200).render('base', {
+        tour: 'The Forest Hiker',
+        user:'Khang Tran'
+    });
 });
 
 app.use('/api/v1/tours', tourRouter);
